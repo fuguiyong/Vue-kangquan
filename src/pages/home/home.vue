d1af4ac3179ebdf3b592b0c60ce9186b0bde6309
fugui <fuguiyong@163.com>
2018-12-09 21:14:29 +0800

    knagquan init

---
+++ src/pages/home/home.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <button @click="sweetalert">click me</button>
+    <div class="btn-group"
+         role="group"
+         aria-label="...">
+      <button type="button"
+              class="btn btn-default">Left</button>
+      <button type="button"
+              class="btn btn-default">Middle</button>
+      <button type="button"
+              class="btn btn-default">Right</button>
+    </div>
+  </div>
+</template>
+
+<script>
+$(function () {
+  alert(123);
+});
+export default {
+  name: 'home',
+  methods: {
+    sweetalert (e) {
+      this.$swal('this is sweetalert2')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from '@/pages/home/home'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'Home',
+      component: Home
+    }
+  ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
+})
